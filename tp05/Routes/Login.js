46108e74df295b8a3c19ce53ca7941ebdb27c0f1
Enfasys01 <102042337+Enfasys01@users.noreply.github.com>
2022-08-04 09:05:51 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ tp05/Routes/Login.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+import axios from 'axios'
+const client = axios.create({baseURL:'aaaaaaa'})
+export default function Login(){
+    
+    return(<Text>aaaaaaaa</Text>)
+}
